03bce5fb2ae0999fcc86705fde4fd521cf6bcf33
Dan Kroszner <dankroz@gmail.com>
2019-06-08 14:04:01 -0400

    update

---
+++ app.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    var clicked = false;
+    // hamburger menu
+    $("#nav-toggle").on("click", function () {
+        console.log("hello")
+        this.classList.toggle("active");
+        if (clicked === true) {
+            console.log("clicked is true");
+            closeNav();
+        }
+        else {
+            console.log("clicked is false"); 
+            openNav();
+        }
+
+    });
+
+    function openNav() {
+        $("#myNav").css("display", "block");
+        clicked = true;
+        $('.navvy').each(function(){
+            $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+        });
+    
+        anime.timeline().add({
+            targets: '.navvy .letter',
+            translateY: [100,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 1000,
+            delay: function(el, i) {
+                return 300 + 30 * i;
+            }
+        })
+    }
+
+    function closeNav() {
+        $('.navvy').each(function(){
+            $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+        });
+        anime.timeline().add({
+            targets: '.navvy .letter',
+            translateY: [0,-100],
+            opacity: [1,0],
+            easing: "easeInExpo",
+            duration: 1200,
+            delay: function(el, i) {
+              return 100 + 30 * i;
+            }
+        });
+        $("#myNav").css("display", "none");
+        clicked = false;
+    }
+
+
+    
+
+
+
+});
